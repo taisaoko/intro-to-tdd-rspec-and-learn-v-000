ffbd73af3973e9a54db0a188c2a7771a68ff2313
taisaoko <ttd3calvin@gmail.com>
2018-07-14 19:55:11 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,6 +6,7 @@ describe "current_age_for_birth_year method" do
     birth_year = 1984
     answer = current_year - birth_year
 
+    age_of_person = current_age_for_birth_year(birth_year)
     expect(age_of_person).to eq(answer)
   end
 end
